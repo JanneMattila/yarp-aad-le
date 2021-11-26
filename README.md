801e23a44a4cd67188e12154877f28510d9babd8
Janne Mattila <janne.mattila@hotmail.fi>
2021-11-26 15:04:40 +0200

    Add support for Azure AD authentication

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # YARP + Azure AD + Let's Encrypt
 
 YARP configured with Azure AD authentication and Let's Encrypt certificates
+
+## Azure AD Authentication
+
+Follow these [instructions](https://docs.microsoft.com/en-us/azure/active-directory/develop/quickstart-register-app)
+and set redirect uri to be: `https://<youraddress>/signin-oidc` and remember to enable `ID tokens` under authentication.
--- src/YARP-AAD-LE/Program.cs
+++ src/YARP-AAD-LE/Program.cs
@@ -1,12 +1,18 @@
-using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Identity.Web;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddReverseProxy()
-    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+builder.Services.AddMicrosoftIdentityWebAppAuthentication(builder.Configuration);
+builder.Services.AddAuthorization(options =>
+{
+    options.FallbackPolicy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+});
 
 var app = builder.Build();
-
+app.UseAuthentication();
 app.MapReverseProxy();
 
 await app.RunAsync();
