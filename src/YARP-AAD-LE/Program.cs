12834c78c07eb87aa2d1bcf1fd99e28fd0bb2aad
Janne Mattila <janne.mattila@hotmail.fi>
2023-01-26 17:43:58 +0200

    Update to .NET 7 and make features optional

--- src/YARP-AAD-LE/Program.cs
+++ src/YARP-AAD-LE/Program.cs
@@ -2,33 +2,51 @@ using Azure.Identity;
 using Microsoft.Identity.Web;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.WebHost.ConfigureKestrel(serverOptions =>
-{
-    serverOptions.ConfigureHttpsDefaults(configureOptions =>
-    {
-        var appServices = serverOptions.ApplicationServices;
-        configureOptions.UseLettuceEncrypt(appServices);
-    });
-});
-
 var keyvault = builder.Configuration["Keyvault"];
-if(!string.IsNullOrEmpty(keyvault))
+
+var isKeyVaultConfigured = keyvault != null;
+if (isKeyVaultConfigured)
 {
-    // Key Vault configured, so pull configuration from there
+    // Key Vault configured
+    builder.WebHost.ConfigureKestrel(serverOptions =>
+    {
+        serverOptions.ConfigureHttpsDefaults(configureOptions =>
+        {
+            var appServices = serverOptions.ApplicationServices;
+            configureOptions.UseLettuceEncrypt(appServices);
+        });
+    });
+
     builder.Configuration.AddAzureKeyVault(new Uri(keyvault), new DefaultAzureCredential());
+    builder.Services.AddLettuceEncrypt().PersistCertificatesToAzureKeyVault();
 }
 
+
 builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
-builder.Services.AddLettuceEncrypt().PersistCertificatesToAzureKeyVault();
-builder.Services.AddMicrosoftIdentityWebAppAuthentication(builder.Configuration);
-builder.Services.AddAuthorization(options =>
+
+var isAzureADConfigured = builder.Configuration.GetSection("AzureAd").Value != null;
+if (isAzureADConfigured)
 {
-    options.AddPolicy("proxyPolicy", policy => policy.RequireAuthenticatedUser());
-});
+    builder.Services.AddMicrosoftIdentityWebAppAuthentication(builder.Configuration);
+    builder.Services.AddAuthorization(options =>
+    {
+        options.AddPolicy("proxyPolicy", policy => policy.RequireAuthenticatedUser());
+    });
+}
+
 var app = builder.Build();
-app.UseHttpsRedirection();
-app.UseAuthentication();
-app.UseAuthorization();
+
+if (isKeyVaultConfigured)
+{
+    app.UseHttpsRedirection();
+}
+
+if (isAzureADConfigured)
+{
+    app.UseAuthentication();
+    app.UseAuthorization();
+}
+
 app.MapReverseProxy();
 
 await app.RunAsync();
